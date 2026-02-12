49b9978d419d608c28285570b0b7997de688e3e0
GenericGamertagg <5018761@stu.highlineschools.org>
2026-02-12 10:21:03 -0800

    slightly fix animation

--- My project/Assets/Scripts/Spike trigger.cs
+++ My project/Assets/Scripts/Spike trigger.cs
@@ -5,17 +5,17 @@ public class Animationplayer : MonoBehaviour
     [SerializeField] private Animator targetAnimator;
     [SerializeField] private string triggerName = "PlayAnim";
 
-    private bool hasPlayed = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Triggered by: " + other.name);
         Debug.Log("broken");
-        if (hasPlayed) return;
 
         if (other.CompareTag("Player"))
         {
+            Debug.Log("something ain't workin");
             targetAnimator.SetTrigger(triggerName);
-            hasPlayed = true;
         }
+    
     }
 }
